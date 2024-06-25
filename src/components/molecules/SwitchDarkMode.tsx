db0e5845729b6d82f7a590d231344c7de906029b
marie <mje.espinosa@gmail.com>
2024-06-25 14:50:08 +0200

    align center the switch in header

--- src/components/molecules/SwitchDarkMode.tsx
+++ src/components/molecules/SwitchDarkMode.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import { useTheme } from '../../utils/hooks/useTheme';
 import { themes } from '../../configs/themeConfigs';
 import Switch from '../atoms/Switch';
 
+// Render switch that handle light/dark mode
 const SwitchDarkMode = () => {
   const [theme, handleChange] = useTheme(themes.darkTheme);
   return (
